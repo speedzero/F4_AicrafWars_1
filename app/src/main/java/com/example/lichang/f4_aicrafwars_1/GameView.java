4e15e1de549d731805ce5086b95acb8a52086d37
speedzero <939836402@qq.com>
2019-03-24 19:23:19 +0800

    提交项目

---
+++ app/src/main/java/com/example/lichang/f4_aicrafwars_1/GameView.java
@@ -0,0 +1,36 @@
+package com.example.lichang.f4_aicrafwars_1;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable,SurfaceHolder.Callback {
+    public GameView(Context context) {
+        super(context);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- app/src/main/java/com/example/lichang/f4_aicrafwars_1/MainActivity.java
+++ app/src/main/java/com/example/lichang/f4_aicrafwars_1/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.lichang.f4_aicrafwars_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt_Start ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt_Start = findViewById(R.id.bt_start);
+
+        bt_Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+
 }
